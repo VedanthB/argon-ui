41d2e7597559c941029182cb53c5404d4a6a692c
Vedanth Bora <vedanthbora@gmail.com>
2022-01-23 13:23:24 +0530

    simple card with image

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -57,6 +57,7 @@ export const SimpleCard = () => {
         }}
       >
         <Button
+          onClick={() => console.log("clicked")}
           style={{
             margin: "0",
           }}
@@ -64,6 +65,51 @@ export const SimpleCard = () => {
         >
           Learn More
         </Button>
+      </div>
+    </Card>
+  );
+};
+
+export const SimpleImageCard = () => {
+  return (
+    <Card
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "345px",
+        height: "320px",
+        overflow: "hidden",
+      }}
+    >
+      <Card.Img width="345" height="140" src="https://picsum.photos/345/140" />
+      <div
+        style={{
+          height: "180px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          padding: " 1rem",
+        }}
+      >
+        <Card.Title
+          style={{
+            fontSize: "24px",
+            fontWeight: "400",
+            margin: "0px",
+            marginBottom: "24px",
+          }}
+        >
+          Lizard
+        </Card.Title>
+        <Card.Text
+          style={{
+            margin: "0px",
+          }}
+        >
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Card.Text>
       </div>
     </Card>
   );
